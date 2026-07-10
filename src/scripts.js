95e63e9a243551654a584546c480532bec736e7e
jshonofre76-wq <jshonofre76@gmail.com>
2026-07-09 20:14:52 -0500

    prueba

---
+++ src/scripts.js
@@ -0,0 +1,5 @@
+function activarEjercicio(tema) {
+    // Aquí puedes agregar la lógica para activar el ejercicio correspondiente
+    console.log(`Activando ejercicio de ${tema}`);
+    // Puedes mostrar un modal, cargar contenido dinámico, etc.
+}
