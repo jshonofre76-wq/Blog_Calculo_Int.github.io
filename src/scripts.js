81f5674251c85d0932bdde4c88fb59e5e705aa61
jshonofre76-wq <jshonofre76@gmail.com>
2026-07-09 22:39:49 -0500

    proyecto actualizado

--- src/scripts.js
+++
@@ -1,5 +0,0 @@
-function activarEjercicio(tema) {
-    // Aquí puedes agregar la lógica para activar el ejercicio correspondiente
-    console.log(`Activando ejercicio de ${tema}`);
-    // Puedes mostrar un modal, cargar contenido dinámico, etc.
-}
